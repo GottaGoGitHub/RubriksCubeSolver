41e7739929e2231b79abc9be69ca40e7a11844ea
JennyDoering <jenny.doering@uni-weimar.de>
2020-06-02 19:05:03 +0200

    added all rotate_cube_* functions + added rotate_right()

--- cube.py
+++ cube.py
@@ -26,24 +26,24 @@ class RubriksCube:
             print()
 
     def rotate_front(self):
-        redtemp0 = self.cube[1][0]
-        redtemp1 = self.cube[1][1]
-        redtemp2 = self.cube[1][2]
-        redtemp3 = self.cube[1][3]
-        redtemp4 = self.cube[1][4]
-        redtemp5 = self.cube[1][5]
-        redtemp6 = self.cube[1][6]
-        redtemp7 = self.cube[1][7]
-        redtemp8 = self.cube[1][8]
-        self.cube[1][0] = redtemp6
-        self.cube[1][1] = redtemp3
-        self.cube[1][2] = redtemp0
-        self.cube[1][3] = redtemp7
-        self.cube[1][4] = redtemp4
-        self.cube[1][5] = redtemp1
-        self.cube[1][6] = redtemp8
-        self.cube[1][7] = redtemp5
-        self.cube[1][8] = redtemp2
+        fronttemp0 = self.cube[1][0]
+        fronttemp1 = self.cube[1][1]
+        fronttemp2 = self.cube[1][2]
+        fronttemp3 = self.cube[1][3]
+        fronttemp4 = self.cube[1][4]
+        fronttemp5 = self.cube[1][5]
+        fronttemp6 = self.cube[1][6]
+        fronttemp7 = self.cube[1][7]
+        fronttemp8 = self.cube[1][8]
+        self.cube[1][0] = fronttemp6
+        self.cube[1][1] = fronttemp3
+        self.cube[1][2] = fronttemp0
+        self.cube[1][3] = fronttemp7
+        self.cube[1][4] = fronttemp4
+        self.cube[1][5] = fronttemp1
+        self.cube[1][6] = fronttemp8
+        self.cube[1][7] = fronttemp5
+        self.cube[1][8] = fronttemp2
         obentemp1 = self.cube[0][6]
         obentemp2 = self.cube[0][7]
         obentemp3 = self.cube[0][8]
@@ -69,12 +69,121 @@ class RubriksCube:
         self.cube[4][5] = untentemp2
         self.cube[4][8] = untentemp3
 
+    def rotate_cube_right(self):
+        fronttemp = self.cube[1]
+        righttemp = self.cube[2]
+        backtemp = self.cube[3]
+        lefttemp = self.cube[4]
+        uptemp0 = self.cube[0][0]
+        uptemp1 = self.cube[0][1]
+        uptemp2 = self.cube[0][2]
+        uptemp3 = self.cube[0][3]
+        uptemp4 = self.cube[0][4]
+        uptemp5 = self.cube[0][5]
+        uptemp6 = self.cube[0][6]
+        uptemp7 = self.cube[0][7]
+        uptemp8 = self.cube[0][8]
+        downtemp0 = self.cube[5][0]
+        downtemp1 = self.cube[5][1]
+        downtemp2 = self.cube[5][2]
+        downtemp3 = self.cube[5][3]
+        downtemp4 = self.cube[5][4]
+        downtemp5 = self.cube[5][5]
+        downtemp6 = self.cube[5][6]
+        downtemp7 = self.cube[5][7]
+        downtemp8 = self.cube[5][8]
+        self.cube[2] = fronttemp
+        self.cube[3] = righttemp
+        self.cube[4] = backtemp
+        self.cube[1] = lefttemp
+        self.cube[0][0] = uptemp2
+        self.cube[0][1] = uptemp5
+        self.cube[0][2] = uptemp8
+        self.cube[0][3] = uptemp1
+        self.cube[0][4] = uptemp4
+        self.cube[0][5] = uptemp7
+        self.cube[0][6] = uptemp0
+        self.cube[0][7] = uptemp3
+        self.cube[0][8] = uptemp6
+        self.cube[5][0] = downtemp6
+        self.cube[5][1] = downtemp3
+        self.cube[5][2] = downtemp0
+        self.cube[5][3] = downtemp7
+        self.cube[5][4] = downtemp4
+        self.cube[5][5] = downtemp1
+        self.cube[5][6] = downtemp8
+        self.cube[5][7] = downtemp5
+        self.cube[5][8] = downtemp2
+
+    def rotate_cube_left(self):
+        self.rotate_cube_right()
+        self.rotate_cube_right()
+        self.rotate_cube_right()
+
+    def rotate_cube_up(self):
+        uptemp = self.cube[0]
+        fronttemp = self.cube[1]
+        backtemp = self.cube[3]
+        downtemp = self.cube[5]
+        righttemp0 = self.cube[2][0]
+        righttemp1 = self.cube[2][1]
+        righttemp2 = self.cube[2][2]
+        righttemp3 = self.cube[2][3]
+        righttemp4 = self.cube[2][4]
+        righttemp5 = self.cube[2][5]
+        righttemp6 = self.cube[2][6]
+        righttemp7 = self.cube[2][7]
+        righttemp8 = self.cube[2][8]
+        lefttemp0 = self.cube[4][0]
+        lefttemp1 = self.cube[4][1]
+        lefttemp2 = self.cube[4][2]
+        lefttemp3 = self.cube[4][3]
+        lefttemp4 = self.cube[4][4]
+        lefttemp5 = self.cube[4][5]
+        lefttemp6 = self.cube[4][6]
+        lefttemp7 = self.cube[4][7]
+        lefttemp8 = self.cube[4][8]
+        self.cube[0] = fronttemp
+        self.cube[1] = downtemp
+        self.cube[5] = backtemp
+        self.cube[3] = uptemp
+        self.cube[2][0] = righttemp6
+        self.cube[2][1] = righttemp3
+        self.cube[2][2] = righttemp0
+        self.cube[2][3] = righttemp7
+        self.cube[2][4] = righttemp4
+        self.cube[2][5] = righttemp1
+        self.cube[2][6] = righttemp8
+        self.cube[2][7] = righttemp5
+        self.cube[2][8] = righttemp2
+        self.cube[4][0] = lefttemp2
+        self.cube[4][1] = lefttemp5
+        self.cube[4][2] = lefttemp8
+        self.cube[4][3] = lefttemp1
+        self.cube[4][4] = lefttemp4
+        self.cube[4][5] = lefttemp7
+        self.cube[4][6] = lefttemp0
+        self.cube[4][7] = lefttemp3
+        self.cube[4][8] = lefttemp6
+
+    def rotate_cube_down(self):
+        self.rotate_cube_up()
+        self.rotate_cube_up()
+        self.rotate_cube_up()
+
+
+    def rotate_right(self):
+        self.rotate_cube_left()
+        self.rotate_front()
+        self.rotate_cube_right()
+
 
 def main():
     cube = RubriksCube()
     cube.print_cube()
     print()
-    cube.rotate_front()
+    # cube.rotate_front()
+    cube.rotate_right()
     cube.print_cube()
 
 
