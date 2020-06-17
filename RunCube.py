264c8fdbf3a7e701a1f25876970950b601a6b2d1
nire2864 <luigi.vinzenz.portwich@uni-weimar.de>
2020-06-17 19:43:58 +0200

    [ADD] rotation buttons and keystroke events

--- RunCube.py
+++ RunCube.py
@@ -18,7 +18,7 @@ text_font_bold = Font(family="Times New Roman", size=10, weight="bold")
 
 # window defines the canvas on which the hexomino will be drawn
 window = Canvas(root, width=500, height=500, bg="grey")
-window.grid(row=5, column=0)
+window.grid(row=5, column=0, columnspan=5, sticky=W)
 
 
 # Creation of the cube
@@ -30,18 +30,18 @@ gernerate_prompt(window, text_font, text_font_bold)
 
 # Creating the questions and input fields
 question1 = Label(root,  font=text_font, text="Which side do you want to modify?")
-question1.grid(row=0, column=0)
+question1.grid(row=0, column=0, columnspan=5)
 
 answer1 = Entry(root)
 answer1.configure(width=30)
-answer1.grid(row=1, column=0)
+answer1.grid(row=1, column=0, columnspan=5)
 
 question2 = Label(root, font=text_font, text="How do you want to color your pieces? Please enter the number followed by the color. ")
-question2.grid(row=2, column=0)
+question2.grid(row=2, column=0, columnspan=5)
 
 answer2 = Entry(root)
 answer2.configure(width=30)
-answer2.grid(row=3, column=0)
+answer2.grid(row=3, column=0, columnspan=5)
 
 
 # Binding the ENTER Key as event to the Entries "answer1" and "answer2"
@@ -53,8 +53,69 @@ answer1.bind("<Return>", press_enter)
 answer2.bind("<Return>", press_enter)
 
 
-#TEST
-button_rotate = Button(root, text="Test Rotate", command=rotate_cube_up)
-button_rotate.grid(row=6, column=0)
+# Creating the Rotation buttons
+def rotate_up_for_button():
+    rotate_cube_up(window, cube)
+
+
+button_rotate_up = Button(root, text="Up", command=rotate_up_for_button)
+button_rotate_up.configure(width=5)
+button_rotate_up.grid(row=6, column=2, sticky=S)
+
+
+def rotate_down_for_button():
+    rotate_cube_down(window, cube)
+
+
+button_rotate_down = Button(root, text="Down", command=rotate_down_for_button)
+button_rotate_down.configure(width=5)
+button_rotate_down.grid(row=8, column=2, sticky=N)
+
+
+def rotate_left_for_button():
+    rotate_cube_left(window, cube)
+
+
+button_rotate_left = Button(root, text="Left", command=rotate_left_for_button)
+button_rotate_left.configure(width=5)
+button_rotate_left.grid(row=7, column=1, sticky=E)
+
+
+def rotate_right_for_button():
+    rotate_cube_right(window, cube)
+
+
+button_rotate_right = Button(root, text="Right", command=rotate_right_for_button)
+button_rotate_right.configure(width=5)
+button_rotate_right.grid(row=7, column=3, sticky=W)
+
+
+# Binding the arrow key to the corresponding rotation
+def rotate_up_for_key(event):
+    rotate_cube_up(window, cube)
+
+
+root.bind('<Up>', rotate_up_for_key)
+
+
+def rotate_down_for_key(event):
+    rotate_cube_down(window, cube)
+
+
+root.bind('<Down>', rotate_down_for_key)
+
+
+def rotate_left_for_key(event):
+    rotate_cube_left(window, cube)
+
+
+root.bind('<Left>', rotate_left_for_key)
+
+
+def rotate_right_for_key(event):
+    rotate_cube_left(window, cube)
+
+
+root.bind('<Right>', rotate_right_for_key)
 
 root.mainloop()
