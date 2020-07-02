6ac12ea627503a2316704643a6185f21691ce0c1
nire2864 <luigi.vinzenz.portwich@uni-weimar.de>
2020-07-02 16:49:17 +0200

    [ADD] Import and Export from/to csv file

--- FileHandler.py
+++ FileHandler.py
@@ -2,6 +2,12 @@ from cubies import *
 
 
 def import_cube_from_csv(filepath):
+    """
+    imports a cube form a csv file
+
+    the file consists of one header row (Line 1), 26 cubies with their values
+    and an empty line (Line 28) and 14 columns whith data
+    """
     filehandler = open(filepath)
 
     content = filehandler.read()
@@ -9,6 +15,7 @@ def import_cube_from_csv(filepath):
     cubies = []
 
     rows = content.split('\n')
+
     for i in range(1, len(rows)-2):
         temp = rows[i].split(',')
 
@@ -24,4 +31,38 @@ def import_cube_from_csv(filepath):
                            [int(temp[11]), int(temp[12])],
                            temp[13])
         cubies.append(temp_cubie)
+
+    filehandler.close()
     return cubies
+
+
+def export_cube_to_csv(cubies_list, filepath):
+    """
+    exports a cube with all its values to a csv file
+    """
+    filehandler = open(filepath, "w")
+    header = "Cubies N,Colors number,Color 1,Pos Color 1 x,Pos Color 1 y," \
+             "Identifier,Color 2,Pos Color 2 x,Pos Color 2 y,Identifier,Color 3" \
+             ",Pos Color 3 x,Pos Color 3 y,Identifier"
+    filehandler.write(header)
+
+    for cubie in cubies_list:
+        temp = cubie.name + ","
+        temp += str(cubie.number) + ","
+        temp += cubie.color1 + ","
+        temp += str(cubie.pos1[0]) + ","
+        temp += str(cubie.pos1[1]) + ","
+        temp += cubie.id1 + ","
+        temp += cubie.color2 + ","
+        temp += str(cubie.pos2[0]) + ","
+        temp += str(cubie.pos2[1]) + ","
+        temp += cubie.id2 + ","
+        temp += cubie.color3 + ","
+        temp += str(cubie.pos3[0]) + ","
+        temp += str(cubie.pos3[1]) + ","
+        temp += cubie.id3 + "\n"
+        filehandler.write(temp)
+
+    endl = ",,,,,,,,,,,,,"
+    filehandler.write(endl)
+    filehandler.close()
