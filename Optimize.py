ffe8703bc6a6679210ea6336114150712d8155d7
JennyDoering <jenny.doering@uni-weimar.de>
2020-08-17 19:40:54 +0200

    [ADD] some comments in the optimize function

--- Optimize.py
+++ Optimize.py
@@ -3,7 +3,15 @@ from Solver import *
 def optimize_solver(rotations):
     print("hallo")
     
+    # changing for example R R R to R' etc.
+    # for doing 2 less steps for each 
+
     i = len(rotations) - 1
+
+    # we iterate from the end of the array to the begin of the array
+    # because the length of the array decreases in every step we change
+    # for example R R R to R'
+    # so we do not have a fixed length 
 
     while (i != 2):
         if (rotations[i] == rotations[i - 1]) and (rotations[i - 1] == rotations[i - 2]):
@@ -60,7 +68,14 @@ def optimize_solver(rotations):
                 rotations[i - 2] = "D"
 
         i = i - 1 
-   
+
+    # TODO:
+    # delete for example R R' from the array 
+    # (R R' is not nessessary, because it happens nothing)
+
+
+
+
 
 def test_function():
     temp = ["0", "1", "2", "3", "4", "5"]
@@ -81,6 +96,9 @@ def test_function():
     temp[i - 2] = "8"
     print(temp)
 
+
+# TODO: optimize algorithm for the robot, so that the whole cube
+# is not rotated anymore
 
 def optimize_for_robot(rotations):
     print("hi")
